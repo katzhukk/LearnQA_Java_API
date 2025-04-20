94397da24666556c1964a9786c5646b7a626437d
katzhukk <katzhukova230@gmail.com>
2025-04-20 15:45:24 +0300

    update Exercise6.java

--- src/test/java/Exercise6.java
+++ src/test/java/Exercise6.java
@@ -1,10 +1,7 @@
 import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class Exercise6 {
 
